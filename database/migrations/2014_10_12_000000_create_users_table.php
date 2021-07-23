718ed64121a80a7e84d95b183f07ae079e070866
imhasibulhasan <hashibul218@gmail.com>
2021-07-23 10:05:41 +0600

    add users & role migration table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,13 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('role_id')->default(2);
+            $table->string('email')->unique()->nullable();
+            $table->string('phone_number')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('photo')->default("avater.jpg");
+            $table->boolean('status')->default(true);
             $table->rememberToken();
             $table->timestamps();
         });
