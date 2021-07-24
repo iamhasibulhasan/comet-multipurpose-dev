32502d4ac19b6b963332c823f65a20f6f171ef40
imhasibulhasan <hashibul218@gmail.com>
2021-07-24 10:15:45 +0600

    Add logout in our dashboard

---
+++ public/admin/assets/js/comet/custom.js
@@ -0,0 +1,19 @@
+(function ($){
+    $(document).ready(function (){
+
+
+    //    Logout Features
+        $(document).on('click', '#logout_btn', function (e){
+            e.preventDefault();
+            $('#logout_form').submit();
+        });
+
+
+
+
+
+
+
+    });
+
+})(jQuery)
